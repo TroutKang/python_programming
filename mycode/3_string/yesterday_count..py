42cc48e54ee2add342bef5d677b9d208a8815fc8
TroutKang <daechu@kakao.com>
2021-11-16 10:35:15 +0900

    commit file

---
+++ mycode/3_string/yesterday_count..py
@@ -0,0 +1,23 @@
+"""
+yesterday.txt 파일을 읽어서
+yesterday 단어가 몇 번 나오는지를 count 해보기
+
+opem mode
+r : read, w : write
+rb : read binary, wb : write binary
+a : append
+"""
+def file_read(file_name):
+    with open(file_name,"r", encoding='utf-8') as file:
+        lyric = file.read()
+        return lyric
+
+
+read = file_read("yesterday.txt")
+print(read)
+n_of_YESTERDAY = read.upper().count("YESTERDAY")
+print(f'Nuber of a word YESTERDAY {n_of_YESTERDAY}')
+n_of_Yesterday = read.count("Yesterday")
+print(f'Nuber of a word Yesterday {n_of_Yesterday}')
+n_of_yesterday = read.count("yesterday")
+print(f'Nuber of a word yesterday {n_of_yesterday}')
